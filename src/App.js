c125794c32ecbb6c9004f1eeb9d96f787926986f
Lucas Mendes <lucaslsbh1@gmail.com>
2020-05-09 18:55:49 -0300

    :sparkles: added the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import Login from "./pages/Login"
+import React from 'react'
+import { Router } from '@reach/router'
+
+import Login from  './pages/Login'
+import Sales from './pages/Sales'
+import Analytics from './pages/Analytics'
 
 function App() {
   return (
-    <Login />
+    <>
+      <Router>
+        <Sales path="/" />
+        <Login path="/login" />
+        <Analytics path="/graficos" />
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Sales/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function Sales() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Sales;
