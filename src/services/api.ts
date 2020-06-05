95f08e030ee31291863d3e265021352968c749bf
Lucas Mendes <lucaslsbh1@gmail.com>
2020-06-05 09:12:25 -0300

    :clown_face: added api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
   // @TODO => Add into .env
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://bffd6b9eacba.ngrok.io',
+  baseURL: 'http://134.122.112.107',
 })
