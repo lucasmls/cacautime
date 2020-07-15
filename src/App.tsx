a37d9f7527f6ec2302962789a22290c3b6b3c352
Lucas Mendes <lucaslsbh1@gmail.com>
2020-07-14 21:57:44 -0300

    :sparkles: extract routes to a separate file and using public and private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil'
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { calendarOutline, personOutline } from 'ionicons/icons';
+import { IonApp } from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,42 +21,13 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-/* Pages */
-import Duties from './pages/Duties';
-import Customers from './pages/Customers'
-import Candies from './pages/Candies'
-import Duty from './pages/Duty';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <RecoilRoot>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route path="/duties" component={Duties} exact={true} />
-            <Route path="/duty/:id" component={Duty} exact={true} />
-            <Route path="/customers" component={Customers} exact={true} />
-            <Route path="/candies" component={Candies} exact={true} />
-            <Route path="/" render={() => <Redirect to="/duties" />} exact={true} />
-          </IonRouterOutlet>
-
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="customers" href="/customers">
-              <IonIcon icon={personOutline} />
-              <IonLabel>Clientes</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="duties" href="/duties">
-              <IonIcon icon={calendarOutline} />
-              <IonLabel>Plantões</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="candies" href="/candies">
-              <IonIcon src='https://image.flaticon.com/icons/svg/2913/2913787.svg' />
-              <IonLabel>Doces</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </RecoilRoot>
-    </IonReactRouter>
+    <RecoilRoot>
+      <Routes />
+    </RecoilRoot>
   </IonApp>
 );
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Redirect, Switch } from 'react-router-dom';
+import {
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { calendarOutline, personOutline } from 'ionicons/icons';
+
+import PrivateRoute from './private'
+import PublicRoute from './public'
+
+import Login from '../pages/Login';
+import Duties from '../pages/Duties';
+import Duty from '../pages/Duty';
+import Customers from '../pages/Customers';
+import Candies from '../pages/Candies';
+
+import { useAuth } from '../hooks';
+
+const Routes: React.FC = () => {
+  const [{ isSignedIn }] = useAuth();
+
+  return (
+    <IonReactRouter>
+      <Switch>
+        <PublicRoute path='/login' component={Login} />
+
+        <IonTabs>
+          <IonRouterOutlet>
+            <PrivateRoute path="/duties" component={Duties} />
+            <PrivateRoute path="/duty/:id" component={Duty} exact={true} />
+            <PrivateRoute path="/customers" component={Customers} exact={true} />
+            <PrivateRoute path="/candies" component={Candies} exact={true} />
+            <PrivateRoute path="/" render={() => <Redirect to="/duties" />} exact={true} />
+          </IonRouterOutlet>
+      
+          <IonTabBar style={{ display: isSignedIn ? 'flex' : 'none'  }}  slot="bottom">
+            <IonTabButton tab="customers" href="/customers">
+              <IonIcon icon={personOutline} />
+              <IonLabel>Clientes</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="duties" href="/duties">
+              <IonIcon icon={calendarOutline} />
+              <IonLabel>Plantões</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="candies" href="/candies">
+              <IonIcon src='https://image.flaticon.com/icons/svg/2913/2913787.svg' />
+              <IonLabel>Doces</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </Switch>
+
+    </IonReactRouter>
+  )
+}
+
+export default Routes;
