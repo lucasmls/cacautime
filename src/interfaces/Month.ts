ebad948aa85c6100986218df12c39a389decbcdc
Lucas Mendes <lucaslsbh1@gmail.com>
2020-08-26 07:03:43 -0300

    :sparkles: add Month interface

---
+++ src/interfaces/Month.ts
@@ -0,0 +1,6 @@
+
+export interface Month {
+  month: "January" | "February" | "March" | "April" | "May" | "June" | "July" | "August" | "September" | "October" | "November" | "December",
+  year: string,
+  number: string,
+}
